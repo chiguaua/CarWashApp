36b5262b1b1a89786c973cd64ecde5338e61a8e1
Миша <m.bogdanov2@g.nsu.ru>
2022-11-13 17:24:53 +0700

    Registration.....

---
+++ CarWash/app/src/main/java/ru/nsu/carwashapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package ru.nsu.carwashapplication;
+
+import android.annotation.SuppressLint;
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView register;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this,RegisterUser.class));
+                break;
+        }
+    }
+}
---
+++ CarWash/app/src/main/java/ru/nsu/carwashapplication/RegisterUser.java
@@ -0,0 +1,4 @@
+package ru.nsu.carwashapplication;
+
+public class RegisterUser {
+}
