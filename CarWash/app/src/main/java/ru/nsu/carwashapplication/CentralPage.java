104da6fee6a7d2cc7622b1517d1c3002736b4e16
chiguaua <91546000+chiguaua@users.noreply.github.com>
2022-12-02 16:33:46 +0700

    copyWorkApp

--- CarWash/app/src/main/java/ru/nsu/carwashapplication/CentralPage.java
+++ CarWash/app/src/main/java/ru/nsu/carwashapplication/CentralPage.java
@@ -1,10 +1,19 @@
 package ru.nsu.carwashapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +25,9 @@ public class CentralPage extends AppCompatActivity {
 
     RecyclerView offerRecycler;
     OfferAdapter offerAdapter;
+
+    DrawerLayout driverLayout;
+    ActionBarDrawerToggle actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,35 @@ public class CentralPage extends AppCompatActivity {
         offerList.add(new Offer(6, "4x64", "Услуга 6", "2ч", "750 руб", "#DF8500", "Test"));
 
         setOfferRecycler(offerList);
+
+        // Обработка нажатия на картинку menu
+        findViewById(R.id.imageMenu).setOnClickListener(v -> {
+            driverLayout.openDrawer(GravityCompat.START);
+        });
+
+        driverLayout = findViewById(R.id.drawer_layout);
+        actionBar = new ActionBarDrawerToggle(this, driverLayout,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        actionBar.setDrawerIndicatorEnabled(true);
+
+        driverLayout.addDrawerListener(actionBar);
+        actionBar.syncState();
+
+        // Делаю переход через startActivity() -> Возможно сделать как-нибудь изящнее!
+        // Еще каждый переход - отельный Fragment, у меня же - отдельная Activity
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_about_us) {
+                Toast.makeText(getApplicationContext(), "Вы выбрали раздел про нас!", Toast.LENGTH_SHORT).show();
+            }
+            if (id == R.id.nav_main) {
+                Toast.makeText(getApplicationContext(), "Вы выбрали раздел Топаз!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this,CentralPage.class));
+            }
+            return true;
+        });
     }
 
     private void setOfferRecycler(List<Offer> offerList) {
@@ -44,4 +85,8 @@ public class CentralPage extends AppCompatActivity {
         offerRecycler.setAdapter(offerAdapter);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return actionBar.onOptionsItemSelected(item) ||  super.onOptionsItemSelected(item);
+    }
 }
